2360455cb654158aebe4a260608990ed76b0b349
Martín M <skydiver@users.noreply.github.com>
2018-09-23 21:31:17 -0300

    alfred-setapp v1.0.3
    * check if placeholder app exists then open

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const alfy = require('alfy');
 const alfredNotifier = require('alfred-notifier');
+const setapp = require('./lib/setapp');
 const apps = require('./data/apps.json');
 
 alfredNotifier();
 
-const items = alfy
-  .inputMatches(apps, 'name')
-  .map(x => ({
-    title: x.name,
-    subtitle: x.description,
-    arg: x.link,
-    icon: {
-      path: `./icons/${x.icon}`
-    }
-  }));
+(async function() {
 
-alfy.output(items);
+  for (const [key] of Object.entries(apps)) {
+    const path = `/Users/Shared/Setapp/Apps/${apps[key].name}.app`;
+    const is = await setapp.exists(path);
+    apps[key].arg = is ? path : apps[key].link;
+  }
+
+  const items = alfy
+    .inputMatches(apps, 'name')
+    .map(x => ({
+      title: x.name,
+      subtitle: x.description,
+      arg: x.arg,
+      icon: {
+        path: `./icons/${x.icon}`
+      }
+    }));
+
+  alfy.output(items);
+
+}());
